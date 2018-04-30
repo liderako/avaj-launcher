fb4dbe3afe1691d4f57915306efb83c749025563
Anton Sviridov <asvirido@e1r11p9.unit.ua>
2018-04-30 20:35:23 +0300

    add checkLine

--- src/com/student/asvirido/avaj/Validation.java
+++ src/com/student/asvirido/avaj/Validation.java
@@ -2,25 +2,57 @@ package com.student.asvirido.avaj;
 import java.io.*;
 
 public class Validation {
+	// String[3] typeValid;
+
+	public Validation() {
+		// this.typeValid = {"JetPlane", "Helicopter", "Baloon"};
+	}
 
 	public void run(String buffer) {
 		String resSplit[] = buffer.split("\n", 0);
 
 		checkSmallFile(resSplit.length);
-		checkFristLine(resSplit[0]);
+		checkFirstLine(resSplit[0]);
+		checkContainer(resSplit);
 	}
 	private void checkSmallFile(int length) {
 		if (length < 2) {
 			error("Error: Too small file");
 		}
 	}
-	private void checkFristLine(String line) {
+	private void checkFirstLine(String line) {
 		if ((line.matches("\\d+")) == false) {
 			error("Error: Wrong first line");
 		}
 		else if (line.length() == 1) {
 			if ((line.matches("[0]")) == true) {
 				error("Error: small life count");
+			}
+		}
+	}
+	private void checkContainer(String buffer[]) {
+		String typeValid[] = {"JetPlane", "Helicopter", "Baloon"};
+
+		for (int i = 1; i < buffer.length; i++) {
+			String res[] = buffer[i].split(" ");
+			if (res.length != 5) {
+				error("Error size line");
+			}
+			for (int k = 0; k < 3; k++) {
+				if ((res[0].equals(typeValid[k])) == true) {
+					break ;
+				}
+				if (k == 2) {
+					error("Error: wrong type");
+				}
+			}
+			if ((res[1].matches("[A-Z_a-z_0-9]+")) == false) {
+				error("Error: wrong name");
+			}
+			for (int j = 2; j < 5; j++) {
+				if ((res[j].matches("[0-9]+")) == false) {
+					error("Error: invalid digital in LONGITUDE or LATITUDE or HEIGHT");
+				}
 			}
 		}
 	}
